86d727137019d0d6f18bc7fde77066d387a95085
vincent841 <41100208+vincent841@users.noreply.github.com>
2021-02-18 11:11:45 +0900

    remove test codes and add model files for box detection

--- object_tracker/aidobjtrack/objtracking/objecttracking_box.py
+++ object_tracker/aidobjtrack/objtracking/objecttracking_box.py
@@ -14,7 +14,7 @@ from mrcnn import inference as mo
 
 
 def get_line_length(x1, y1, x2, y2):
-    return math.sqrt((x2-x1)**2+(y2-y1)**2)
+    return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
 
 
 def get_line_cross(x11, y11, x12, y12, x21, y21, x22, y22):
@@ -49,38 +49,18 @@ class BoxObject:
 
 
 class BoxObjectTracker(ObjectTracker):
-    # OBJECT_WEIGHT_PATH = "/home/jinwon/Documents/github/object-tracker-python-data/logs/object-train20201017T1422/mask_rcnn_object-train_0043.h5"
-    # OBJECT_WEIGHT_PATH = "/home/jinwon/Documents/github/object-tracker-python-data/logs/object-train20201017T1713/mask_rcnn_object-train_0094.h5"
-    # OBJECT_WEIGHT_PATH = "/home/jinwon/Documents/github/object-tracker-python-data/logs/object-train20201017T2218/mask_rcnn_object-train_0150.h5"
-    # OBJECT_WEIGHT_PATH = "/home/jinwon/Documents/github/object-tracker-python-data/logs/object-train20201018T1104/mask_rcnn_object-train_0200.h5"
-    # OBJECT_WEIGHT_PATH = "/home/jinwon/Documents/github/object-tracker-python-data/logs/object-train20201018T2110/mask_rcnn_object-train_0100.h5"
-    # OBJECT_WEIGHT_PATH = "/home/jinwon/Documents/github/object-tracker-python-data/logs/object-train20201019T1829/mask_rcnn_object-train_0132.h5"
-    # OBJECT_WEIGHT_PATH = "/home/jinwon/Documents/github/object-tracker-python-data/logs/object-train20201022T0658/mask_rcnn_object-train_0080.h5" # bad
-    #OBJECT_WEIGHT_PATH = "/home/jinwon/Documents/github/object-tracker-python-data/logs/object-train20201022T2147/mask_rcnn_object-train_0089.h5"
-    #OBJECT_WEIGHT_PATH = "/home/jinwon/Documents/github/object-tracker-python-data/logs/object-train20201023T0653/mask_rcnn_object-train_0050.h5"
-    #OBJECT_WEIGHT_PATH = "/home/jinwon/Documents/github/object-tracker-python-data/logs/object-train20201023T1430/mask_rcnn_object-train_0093.h5"
-    #OBJECT_WEIGHT_PATH = "/home/jinwon/Documents/github/object-tracker-python-data/logs/object-train20201024T0922/mask_rcnn_object-train_0098.h5"
-    #OBJECT_WEIGHT_PATH = "/home/jinwon/Documents/github/object-tracker-python-data/logs/object-train20201024T2354/mask_rcnn_object-train_0073.h5"
-    #OBJECT_WEIGHT_PATH = "/home/jinwon/Documents/github/object-tracker-python-data/logs/object-train20201025T1253/mask_rcnn_object-train_0099.h5"
-    #OBJECT_WEIGHT_PATH = "/home/jinwon/Documents/github/object-tracker-python/logs/object-train20201030T1653/mask_rcnn_object-train_0188.h5"
-    #OBJECT_WEIGHT_PATH = "/home/jinwon/Documents/github/object-tracker-python/logs/object-train20201030T1653/mask_rcnn_object-train_0137.h5"
-    #OBJECT_WEIGHT_PATH = "/home/jinwon/Documents/github/object-tracker-python/logs/object-train20201031T0859/mask_rcnn_object-train_0120.h5"
-    #OBJECT_WEIGHT_PATH = "/home/jinwon/Documents/github/object-tracker-python/logs/object-train20201031T1858/mask_rcnn_object-train_0180.h5"
-
     # a candidate <-- [base: DelBox2_2] 단일 박스는 잘 인식되는 것으로 보임. 박스의 종류를 늘리면 추가적인 박스에도 적응할 것으로 예상.
-    #OBJECT_WEIGHT_PATH = "/home/jinwon/Documents/github/object-tracker-python-data/logs/object-train20201020T1933/mask_rcnn_object-train_0179.h5"
+    # OBJECT_WEIGHT_PATH = "/home/jinwon/Documents/github/object-tracker-python-data/logs/object-train20201020T1933/mask_rcnn_object-train_0179.h5"
 
     # a valualbe candidate < [base: DelBox8] Box Dataset을 대폭 추가(thanks to NamYW & his friend)
-    #OBJECT_WEIGHT_PATH = "/home/jinwon/Documents/github/object-tracker-python/logs/object-train20201101T1825/mask_rcnn_object-train_0098.h5"
-    #OBJECT_WEIGHT_PATH = "/home/jinwon/Documents/github/object-tracker-python/logs/object-train20201101T1825/mask_rcnn_object-train_0178.h5"
+    # OBJECT_WEIGHT_PATH = "/home/jinwon/Documents/github/object-tracker-python/logs/object-train20201101T1825/mask_rcnn_object-train_0098.h5"
+    # OBJECT_WEIGHT_PATH = "/home/jinwon/Documents/github/object-tracker-python/logs/object-train20201101T1825/mask_rcnn_object-train_0178.h5"
 
     # a best candidate...
-    OBJECT_WEIGHT_PATH = "/home/jinwon/Documents/github/object-tracker-python/logs/object-train20201101T1825/mask_rcnn_object-train_0098.h5"
-
-    # resnet-50 test
-    #OBJECT_WEIGHT_PATH = "/home/jinwon/Documents/github/object-tracker-python/logs/object-train20201122T1517/mask_rcnn_object-train_0059.h5"
-
-    LOGS_PATH = "/home/jinwon/Documents/github/object-tracker-python-data/logs"
+    OBJECT_WEIGHT_PATH = (
+        "./models/object-train20201101T1825/mask_rcnn_object-train_0098.h5"
+    )
+    LOGS_PATH = "./logs"
 
     def __init__(self):
         self.markerObjectList = []
@@ -103,11 +83,12 @@ class BoxObjectTracker(ObjectTracker):
         self.markerObjIDList.clear()
 
         self.maskdetect = mo.MaskRcnnDetect(
-            BoxObjectTracker.OBJECT_WEIGHT_PATH, BoxObjectTracker.LOGS_PATH)
+            BoxObjectTracker.OBJECT_WEIGHT_PATH, BoxObjectTracker.LOGS_PATH
+        )
 
     # set detectable features like marker id of aruco marker
     def setTrackingObject(self, object):
-        assert(isinstance(object, BoxObject))
+        assert isinstance(object, BoxObject)
 
         # TODO: handles two more object here?
         self.markerObjIDList.append(object.markerID)
@@ -130,8 +111,7 @@ class BoxObjectTracker(ObjectTracker):
         ############################################################
         # detect objects
         if self.maskdetect is not None:
-            mask_list = self.maskdetect.detect_object_by_data(
-                color_image)
+            mask_list = self.maskdetect.detect_object_by_data(color_image)
             self.mask_list = mask_list
 
             # get the minArearect and angle for each mask
@@ -139,8 +119,7 @@ class BoxObjectTracker(ObjectTracker):
             print(self.mask_rect_list)
             print(self.mask_angle_list)
 
-            self.center_point_list = self.findBoxCenterList(
-                self.mask_rect_list)
+            self.center_point_list = self.findBoxCenterList(self.mask_rect_list)
             print(self.center_point_list)
 
             # self.center_point_list = self.maskdetect.get_center_points(
@@ -150,7 +129,9 @@ class BoxObjectTracker(ObjectTracker):
             self.scores_list = self.maskdetect.get_scores()
             assert self.scores_list is not None or self.center_point_list is not None
 
-            for idx, (markerObject, cpoint, angle) in enumerate(zip(self.markerObjectList, self.center_point_list, self.mask_angle_list)):
+            for idx, (markerObject, cpoint, angle) in enumerate(
+                zip(self.markerObjectList, self.center_point_list, self.mask_angle_list)
+            ):
                 tvec = vtc.vcap.get_3D_pos(cpoint[0], cpoint[1])
                 # print("---------------------------------------------")
                 # print(vtc.vcap.get_3D_pos(cpoint[0], cpoint[1]))
@@ -176,8 +157,11 @@ class BoxObjectTracker(ObjectTracker):
                 xyzuvw_list = HMUtil.convertHMtoXYZABCDeg(hmResult)
                 xyzuvw_arr = np.array(xyzuvw_list)
 
-                offsetPoint = markerObject.pivotOffset + \
-                    vtc.camObjOffset if vtc.camObjOffset is not None else markerObject.pivotOffset
+                offsetPoint = (
+                    markerObject.pivotOffset + vtc.camObjOffset
+                    if vtc.camObjOffset is not None
+                    else markerObject.pivotOffset
+                )
 
                 xyzuvw_arr += offsetPoint.tolist()
                 xyzuvw = xyzuvw_arr.tolist()
@@ -189,8 +173,8 @@ class BoxObjectTracker(ObjectTracker):
                     # xyzuvw = [x, y, z, u*(-1), v+180.0, w]
                     # NOTE: using the fixed rotation information
                     # TODO: adjust 'w' value by input angle value
-                    angle = angle + 180.0 if angle <= -90.0 else angle     # TEST
-                    xyzuvw = [x, y, z, 180.0, 0.0, 180.0-angle]
+                    angle = angle + 180.0 if angle <= -90.0 else angle  # TEST
+                    xyzuvw = [x, y, z, 180.0, 0.0, 180.0 - angle]
 
                 # set final target position..
                 markerObject.targetPos = xyzuvw
@@ -210,8 +194,7 @@ class BoxObjectTracker(ObjectTracker):
         mask_list = self.mask_list
 
         # get mask image
-        mask_gray_image = self.maskdetect.get_mask_image(
-            mask_list, width, height)
+        mask_gray_image = self.maskdetect.get_mask_image(mask_list, width, height)
         mask_image = cv2.cvtColor(mask_gray_image, cv2.COLOR_GRAY2RGB)
 
         for mask_rect in self.mask_rect_list:
@@ -221,21 +204,21 @@ class BoxObjectTracker(ObjectTracker):
             cv2.drawContours(mask_image, [mask_rect], 0, (255, 0, 0), 2)
 
             # draw center line
-            xx1 = np.int0((mask_rect[0][0] + mask_rect[1][0])/2)
-            yy1 = np.int0((mask_rect[0][1] + mask_rect[1][1])/2)
-            xx2 = np.int0((mask_rect[2][0] + mask_rect[3][0])/2)
-            yy2 = np.int0((mask_rect[2][1] + mask_rect[3][1])/2)
+            xx1 = np.int0((mask_rect[0][0] + mask_rect[1][0]) / 2)
+            yy1 = np.int0((mask_rect[0][1] + mask_rect[1][1]) / 2)
+            xx2 = np.int0((mask_rect[2][0] + mask_rect[3][0]) / 2)
+            yy2 = np.int0((mask_rect[2][1] + mask_rect[3][1]) / 2)
 
             len1 = get_line_length(xx1, yy1, xx2, yy2)
 
-            xx3 = np.int0((mask_rect[0][0] + mask_rect[3][0])/2)
-            yy3 = np.int0((mask_rect[0][1] + mask_rect[3][1])/2)
-            xx4 = np.int0((mask_rect[2][0] + mask_rect[1][0])/2)
-            yy4 = np.int0((mask_rect[2][1] + mask_rect[1][1])/2)
+            xx3 = np.int0((mask_rect[0][0] + mask_rect[3][0]) / 2)
+            yy3 = np.int0((mask_rect[0][1] + mask_rect[3][1]) / 2)
+            xx4 = np.int0((mask_rect[2][0] + mask_rect[1][0]) / 2)
+            yy4 = np.int0((mask_rect[2][1] + mask_rect[1][1]) / 2)
 
             len2 = get_line_length(xx3, yy3, xx4, yy4)
 
-            if(len1 >= len2):
+            if len1 >= len2:
                 color1 = (0, 0, 255)
                 color2 = (0, 255, 0)
             else:
@@ -257,13 +240,29 @@ class BoxObjectTracker(ObjectTracker):
         fontColor2 = (0, 0, 255)
         lineType = 1
 
-        for idx, (cpoint, score, angle) in enumerate(zip(self.center_point_list, self.scores_list, self.mask_angle_list)):
+        for idx, (cpoint, score, angle) in enumerate(
+            zip(self.center_point_list, self.scores_list, self.mask_angle_list)
+        ):
             # print(idx, (cpoint, score))
-            cv2.putText(color_image, '%0.8f' % score, cpoint, font,
-                        fontScale, fontColor, lineType)
+            cv2.putText(
+                color_image,
+                "%0.8f" % score,
+                cpoint,
+                font,
+                fontScale,
+                fontColor,
+                lineType,
+            )
             (xx, yy) = cpoint
-            cv2.putText(color_image, '%0.2f' % angle, (xx, yy+15), font,
-                        fontScale, fontColor2, lineType)
+            cv2.putText(
+                color_image,
+                "%0.2f" % angle,
+                (xx, yy + 15),
+                font,
+                fontScale,
+                fontColor2,
+                lineType,
+            )
 
     def getScoresList(self):
         return self.scores_list
@@ -276,7 +275,8 @@ class BoxObjectTracker(ObjectTracker):
             for mask in mask_list:
                 mask_image = np.where(mask, 255, 0).astype(np.uint8)
                 contours, hierarchy = cv2.findContours(
-                    mask_image, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+                    mask_image, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE
+                )
 
                 if len(contours) > 0:
                     for contour in contours:
@@ -285,8 +285,11 @@ class BoxObjectTracker(ObjectTracker):
 
                         # get rotation angle and push into the list
                         (rectWidth, rectHeight) = areaRect[1]
-                        angle = (areaRect[2] - 90) if(rectWidth <
-                                                      rectHeight) else areaRect[2]
+                        angle = (
+                            (areaRect[2] - 90)
+                            if (rectWidth < rectHeight)
+                            else areaRect[2]
+                        )
                         angle_list.append(angle)
 
                         # get the rectangle coordinates of RotateRect
@@ -296,9 +299,9 @@ class BoxObjectTracker(ObjectTracker):
                         box_list.append(box)
 
                 else:
-                    print('find mask but can not get any contour', file=sys.stderr)
+                    print("find mask but can not get any contour", file=sys.stderr)
         else:
-            print('find mask in mask_list', file=sys.stderr)
+            print("find mask in mask_list", file=sys.stderr)
 
         return (box_list, angle_list)
 
@@ -311,15 +314,15 @@ class BoxObjectTracker(ObjectTracker):
             mask_rect = np.int0(mask_rect)
 
             # get the points of two lines
-            xx1 = np.int0((mask_rect[0][0] + mask_rect[1][0])/2)
-            yy1 = np.int0((mask_rect[0][1] + mask_rect[1][1])/2)
-            xx2 = np.int0((mask_rect[2][0] + mask_rect[3][0])/2)
-            yy2 = np.int0((mask_rect[2][1] + mask_rect[3][1])/2)
+            xx1 = np.int0((mask_rect[0][0] + mask_rect[1][0]) / 2)
+            yy1 = np.int0((mask_rect[0][1] + mask_rect[1][1]) / 2)
+            xx2 = np.int0((mask_rect[2][0] + mask_rect[3][0]) / 2)
+            yy2 = np.int0((mask_rect[2][1] + mask_rect[3][1]) / 2)
 
-            xx3 = np.int0((mask_rect[0][0] + mask_rect[3][0])/2)
-            yy3 = np.int0((mask_rect[0][1] + mask_rect[3][1])/2)
-            xx4 = np.int0((mask_rect[2][0] + mask_rect[1][0])/2)
-            yy4 = np.int0((mask_rect[2][1] + mask_rect[1][1])/2)
+            xx3 = np.int0((mask_rect[0][0] + mask_rect[3][0]) / 2)
+            yy3 = np.int0((mask_rect[0][1] + mask_rect[3][1]) / 2)
+            xx4 = np.int0((mask_rect[2][0] + mask_rect[1][0]) / 2)
+            yy4 = np.int0((mask_rect[2][1] + mask_rect[1][1]) / 2)
 
             (cx, cy) = get_line_cross(xx1, yy1, xx2, yy2, xx3, yy3, xx4, yy4)
             center_point_list.append((np.int0(cx), np.int0(cy)))
